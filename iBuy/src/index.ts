5c4a3c0e1a90c0d5c344a8d25d732254fd077e43
Randall Krauskopf <randall.krauskopf@gmail.com>
2020-07-09 15:40:55 -0500

    Make uuid unique

--- iBuy/src/index.ts
+++ iBuy/src/index.ts
@@ -1,7 +1,7 @@
 import { OrderAppDefinition } from "@shipengine/integration-platform-sdk";
 
 const orderSource: OrderAppDefinition = {
-  id: "5e386891-f693-4cdf-8b0c-82d7eb7542d0",
+  id: "3b76c08d-4299-4333-90bb-cd952bc68525",
   name: "IBuy MarketPlace",
   description: "Welcome to iBuy, the international marketplace for all of your needs.",
   websiteURL: "https://www.iBuy.net",
